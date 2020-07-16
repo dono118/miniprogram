980a40b84789d7d0952f0c2c3373344627338280
dono118 <dono118@126.com>
2020-07-16 15:45:05 +0800

    模块，模板，缓存

--- pages/post/post.js
+++ pages/post/post.js
@@ -1,4 +1,6 @@
 // pages/post/post.js
+import { _DBPost as DBPost } from "../../db/DBPost.js";
+
 Page({
 
   /**
@@ -11,45 +13,11 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var postList = [{
-      obj:{
-        date: "Jan 14 2019",
-      },
-      title: "小时候的冰棍儿与雪糕",
-      postImg: "../images/post/post-4.jpg",
-      avatar: "../images/avatar/avatar-1.png",
-      content: "冰棍与雪糕绝对不是同一个东西。3到5毛钱的雪糕犹如现在的哈根达斯，而5分1毛的 冰棍儿就像现在的老冰棒。时过境迁，...",
-      collectNum: { array: [108] },
-      messageNum: 56,
-      readingNum: 23,
-    },
-      {
-        obj:{
-          date: "Jan 31 2019",
-        },
-        title: "最爱的女孩",
-        postImg: "../images/post/post-5.jpg",
-        avatar: "../images/avatar/avatar-1.png",
-        content: "就让往事随风就随风，心随你动...",
-        collectNum: { array: [356] },
-        messageNum: 234,
-        readingNum: 58,
-      },
-        {
-          obj:{
-            date: "May 14 2019",
-          },
-          title: "Eason 出新歌",
-          postImg: "../images/post/post-6.jpeg",
-          avatar: "../images/avatar/avatar-1.png",
-          content: "如果那两字没有颤动...",
-          collectNum: { array: [10051] },
-          messageNum: 7896,
-          readingNum: 123,
-        }]
+  onLoad: function () {
+    var dbPost = new DBPost();
+    
     this.setData({
-      postDataList: postList
+      postDataList: dbPost.getAllPostData(),
     })
   },
 
