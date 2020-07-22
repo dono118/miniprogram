3dd1acf2b9a366d4c42402fed51460e890841f50
dono118 <dono118@126.com>
2020-07-22 18:27:14 +0800

    文章详情页面

--- pages/post/post-detail/post-detail.js
+++ pages/post/post-detail/post-detail.js
@@ -1,8 +1,4 @@
-// pages/post/post.js
-import {
-  DBPost
-} from "../../db/DBPost.js";
-
+// pages/post/post-detail/post-detail.js
 Page({
 
   /**
@@ -15,20 +11,8 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function () {
-    var dbPost = new DBPost();
-
-    this.setData({
-      postDataList: dbPost.getAllPostData(),
-    })
-  },
-
-  onTapToDetail(event) {
-    var postId = event.currentTarget.dataset.postId;
-    console.log(postId);
-    wx.navigateTo({
-      url: 'post-detail/post-detail?id=' + postId,
-    })
+  onLoad: function (options) {
+    var postId = options.id;
   },
 
   /**
