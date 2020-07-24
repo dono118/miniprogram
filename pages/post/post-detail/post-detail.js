4154984901b7af0153dd698ac8850c08c9358617
dono118 <dono118@126.com>
2020-07-24 15:15:59 +0800

    收藏，点赞

--- pages/post/post-detail/post-detail.js
+++ pages/post/post-detail/post-detail.js
@@ -1,3 +1,7 @@
+import {
+  DBPost
+} from "../../../db/DBPost";
+
 // pages/post/post-detail/post-detail.js
 Page({
 
@@ -13,13 +17,53 @@ Page({
    */
   onLoad: function (options) {
     var postId = options.id;
+    this.dbPost = new DBPost(postId);
+    this.postData = this.dbPost.getPostItemById().data;
+    this.setData({
+      post: this.postData
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    wx.setNavigationBarTitle({
+      title: this.postData.title
+    })
+  },
 
+  onCollectionTap: function () {
+    //dbpost对象已在onLoad函数中被保存到了this变量中，无需再次实例化
+    var newData = this.dbPost.collect();
+
+    //从新绑定数据，注意，不要将整个newData全部作为setData的参数，应当有选择的更新部分数据
+    this.setData({
+      'post.collectionStatus': newData.collectionStatus,
+      'post.collectionNum': newData.collectionNum
+    })
+
+    // 交互反馈
+    wx.showToast({
+      title: newData.collectionStatus ? "收藏成功" : "取消成功",
+      duration: 1000,
+      icon: "success",
+      mask: true
+    })
+  },
+
+  onUpTap:function(){
+    var newData = this.dbPost.up();
+    this.setData({
+      'post.upStatus': newData.upStatus,
+      'post.upNum': newData.upNum
+    })
+    wx.showToast({
+      title: newData.upStatus ? "点赞成功" : "点赞取消",
+      duration: 1000,
+      icon: "success",
+      mask: true
+    })
   },
 
   /**
